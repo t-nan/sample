7a51ada964bd65ba56af42480bff2fbf7753bba3
t-nan <t-nan@yandex.ru>
2020-12-21 22:07:56 +0300

    update validations on User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,9 @@
 class User < ApplicationRecord
-  validates :name, presence: true
+
+  validates :name, presence: true, length: { maximum: 50 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i                     #шаблон из регулярных выражений. VALID_EMAIL_REGEX - константа  
+  validates :email, presence: true, length: { maximum: 255 },
+                                            format: { with: VALID_EMAIL_REGEX },
+                                            uniqueness: { case_sensitive: false }
+                                            
 end
