13e84e29a4f58429c4a1d65f1f935d90999ef66b
t-nan <t-nan@yandex.ru>
2020-12-22 14:13:19 +0300

    add before save filter

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
 
+  before_save { self.email = email.downcase }                                  #перевод email в нижний регистр перед сохранением
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i                     #шаблон из регулярных выражений. VALID_EMAIL_REGEX - константа  
   validates :email, presence: true, length: { maximum: 255 },
